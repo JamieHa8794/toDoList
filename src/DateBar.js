cb2f0ebe505268aede59cd1e9de12affc55f4133
JamieHa8794 <JamieHa8794@gmail.com>
2023-04-05 19:45:43 -0400

    week displayed starting from monday

--- src/DateBar.js
+++ src/DateBar.js
@@ -12,31 +12,62 @@ class DateBar extends Component{
     render(){
         const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         const convertDay = (1000 * 3600 * 24)
-        const date = new Date()
-        const month = months[date.getMonth()];
-        const day = date.getDate();
-        const year = date.getFullYear();
+        const today = new Date()
+        const month = months[today.getMonth()];
+        const day = today.getDate();
+        const year = today.getFullYear();
         // console.log(month, day, year)
         const fullDate = month + ' ' + day + ', ' + year;
-        const today = new Date()
+        // const today = new Date()
 
-        const nextDate = new Date(Number(date))
-        nextDate.setDate(date.getDate() + 1)
-
-        const prevDate = new Date(Number(date))
-        prevDate.setDate(date.getDate() - 1)
+        const prevDate = new Date(Number(today))
+        prevDate.setDate(today.getDate() - 1)
 
 
-        console.log('prevDate', prevDate)
-        console.log('today', today)
-        console.log('nextDate', nextDate)
+        const weekArr = [];
+   
 
+        for(let i = 0; i < today.getDay(); i++){
+            const newNextDate = new Date(Number(today))
+            newNextDate.setDate(today.getDate() - (today.getDay()-i))
+            
+            weekArr.push(newNextDate)
+        }
+
+
+        for(let i = 0; i < (7-today.getDay()); i++){
+            const newNextDate = new Date(Number(today))
+            newNextDate.setDate(today.getDate() + (i))
+            
+            weekArr.push(newNextDate)
+        }
+
+
+        const weekDays = ['Sun','Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat']
 
         return(
             <div className='main-box'>
                 <h1>
                     Date Bar
                 </h1>
+                <h3>
+                    {month}
+                </h3>
+                <div className='week-bar'>
+                    {weekArr.map(day =>{
+                        return(
+                            <div className='week-bar-container'>
+                                <div className='week-bar-weekDay'>
+                                    {weekDays[day.getDay()]}
+                                </div>
+                                <div className='week-bar-date'>
+                                    {day.getDate()}
+                                </div>
+                            </div>
+                        )
+                        
+                    })}
+                </div>
             </div>
         )
     }
